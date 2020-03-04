54f7bbb25c0e5e4f63a2931a62fb99928cf80fdd
zero86 <katanazero86@gmail.com>
2020-03-05 03:52:58 +0900

    [feature] 프로젝트 구성
    - postcss 끄적끄적..

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,11 @@ import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 
-import postcss from 'rollup-plugin-postcss';
+import postcss from 'rollup-plugin-postcss'; // rollup postcss plugin
+import postcssimport from 'postcss-import'; // postcss-import plugin : @import 구문을 사용할 수 있게해줌
+import autoprefixer from 'autoprefixer'; // autoprefixer plugin : css 접두사를 붙여줌
+import tailwindcss from 'tailwindcss'; // tailwindcss plugin
+import purgecss from '@fullhuman/postcss-purgecss'; // postcss-purgecss plugin : PurgeCSS는 사용하지 않는 CSS를 제거하는 도구
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -19,7 +23,18 @@ export default {
 	plugins: [
 
 		postcss({
-			extract: true
+			plugins: [
+				postcssimport(),
+				tailwindcss(),
+				autoprefixer(),
+				purgecss({
+					content: ["**/*.html", "**/*.svelte"],
+					defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+					// fontFace: true,
+					// keyframes: true,
+				}),
+			],
+			// extract: true, // js 파일이 생성된 동일한 위치에 css 파일을 추출
 		}),
 
 		svelte({
