29dfc24b1ff6f4155cfec7fbe0426a87a8fc5e9f
zero86 <katanazero86@gmail.com>
2020-03-10 06:00:44 +0900

    [feature] svelte TODO List app
    - https://tailwindcss.com/docs 참조하여 UI 작업
    - TODO APP 작업.
    - UI 컴포넌트 작업

--- src/App.svelte
+++ src/App.svelte
@@ -3,10 +3,14 @@
     import {FIREBASE_DB} from './firebase';
     import moment from 'moment';
     import 'moment-timezone';
+    import {fade} from 'svelte/transition';
+
     import CardComponent from "./components/card/CardComponent.svelte";
+    import TodoFormModalComponent from "./components/modal/TodoFormModalComponent.svelte";
 
     moment.tz.setDefault("Asia/Seoul");
 
+    let isShow = false;
 
     let list = [];
     FIREBASE_DB.collection("list")
@@ -50,25 +54,30 @@
                     name: '수정',
                     age: 50
                 })
-    }
-
-    const checkEmoji = (e) => {
-
-        const regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
-        if (regex.test(e.target.value)) {
-            e.target.value = e.target.value.replace(regex, '');
-        }
-
     };
 
-    const checkNumber = (e) => {
+    const checkValidation = (() => {
 
-        if (e.keyCode != 8 && !(e.keyCode >= 48 && e.keyCode <= 57) && !(e.keyCode >= 96 && e.keyCode <= 105)) {
-            e.preventDefault();
+        const emojiValidation = (e) => {
+            const regex = /(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|\ud83c[\ude32-\ude3a]|\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])/g;
+            if (regex.test(e.target.value)) {
+                e.target.value = e.target.value.replace(regex, '');
+            }
+        };
+
+        const numberValidation = (e) => {
+            console.log('zzz');
+            if (e.keyCode != 8 && !(e.keyCode >= 48 && e.keyCode <= 57) && !(e.keyCode >= 96 && e.keyCode <= 105)) {
+                e.preventDefault();
+            }
+        };
+
+        return {
+            emojiValidation,
+            numberValidation,
         }
 
-    };
-
+    })();
 
 </script>
 
@@ -84,7 +93,7 @@
         </p>
         <div class="flex items-center justify-end">
             <button class="bg-indigo-600 hover:bg-indigo-800 text-white font-bold p-2 rounded shadow"
-                    on:click={createListItem}>
+                    on:click={() => isShow = true }>
                 Add TODO
             </button>
         </div>
@@ -102,14 +111,14 @@
             name
         </label>
         <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-               type="text" placeholder="ryan" bind:value={addName} on:keyup={checkEmoji}>
+               type="text" placeholder="ryan" bind:value={addName} on:keyup={checkValidation.emojiValidation}>
     </div>
     <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
         <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
             age
         </label>
         <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-               type="text" placeholder="25" bind:value={addAge} on:keydown={checkNumber}>
+               type="text" placeholder="25" bind:value={addAge} on:keydown={checkValidation.numberValidation}>
     </div>
 
     <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" on:click={createListItem}>
@@ -135,6 +144,13 @@
 </div>
 
 <CardComponent/>
+
+
+{#if isShow}
+<div transition:fade="{{delay: 0, duration: 250}}">
+    <TodoFormModalComponent on:cancel={() => isShow = false} />
+</div>
+{/if}
 
 <style>
 
